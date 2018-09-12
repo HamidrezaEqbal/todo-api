f2689a37d9698a8ff50d562a7bb4ace5bbfe3a1a
Hamidreza Eqbal <hehaunted@gmail.com>
2018-09-12 20:30:24 +0430

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,58 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {
+    app
+} = require('./../server');
+const {
+    Todo
+} = require('./../models/todo');
+
+beforeEach((done) => {
+    Todo.deleteMany({}).then(() => done());
+})
+
+describe('POST /todos', () => {
+    it('should add todo to the database', (done) => {
+        var text = 'test todo';
+        request(app)
+            .post('/todos').send({
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                        expect(todos.length).toBe(1);
+                        expect(todos[0].text).toBe(text);
+                        done();
+                    })
+                    .catch((err) => {
+                        done(err);
+                    });
+
+            });
+    });
+
+    it('should not add invalid todo to the database', (done) => {
+        request(app).post('/todos').send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+
+});
