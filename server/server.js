ce7d0741caf6942230b632c03f88f122b14b172f
Hamidreza Eqbal <hehaunted@gmail.com>
2018-09-16 15:56:15 +0430

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ var {
     Todo
 } = require('./models/todo');
 
+var {
+    ObjectID
+} = require('mongodb');
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -35,7 +39,22 @@ app.get('/todos', (req, res) => {
     });
 });
 
-
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
