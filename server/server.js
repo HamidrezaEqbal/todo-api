aac4332ac7b78948820621fb70efde1b574bd66d
Hamidreza Eqbal <hehaunted@gmail.com>
2018-09-18 01:00:55 +0430

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const {
     mongoose
 } = require('./db/mongoose');
+
 const {
     User
 } = require('./models/user');
@@ -11,10 +13,12 @@ const {
     Todo
 } = require('./models/todo');
 
-
 const {
     ObjectID
 } = require('mongodb');
+
+// to solve DeprecationWarning:
+mongoose.set('useFindAndModify', false);
 
 const PORT = process.env.PORT || 3000;
 
@@ -65,22 +69,7 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
 
-    //The code below causese DeprecationWarning:
-
-    // Todo.findByIdAndRemove(id).then((todo) => {
-    //     if (!todo) {
-    //         return res.status(404).send();
-    //     }
-    //     res.send({
-    //         todo
-    //     });
-    // }).catch((err) => {
-    //     res.status(400).send();
-    // });
-
-    Todo.findOneAndDelete({
-        _id: id
-    }).then((todo) => {
+    Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -91,6 +80,48 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 
+    // Todo.findOneAndDelete({
+    //     _id: id
+    // }).then((todo) => {
+    //     if (!todo) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send({
+    //         todo
+    //     });
+    // }).catch((err) => {
+    //     res.status(400).send();
+    // });
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    var body = _.pick(req.body, ['text', 'completed'])
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch((err) => {
+        res.send(400).send();
+    });
 });
 
 app.listen(PORT, () => {
